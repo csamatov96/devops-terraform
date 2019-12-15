5ef2e7dbd4e494f336005595202442ae1b8ec9df
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-15 02:49:33 -0600

    sec added

---
+++ sec_group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "sg" {
+  name        = "custom_sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.custom_vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22 #FROM A
+    to_port     = 22 #TO B
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
