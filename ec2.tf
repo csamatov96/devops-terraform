505a94c842e7d9ab528c0cdf9000b16bed62d1a8
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-15 01:30:58 -0600

    lifecycle added

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,15 @@
 resource "aws_instance" "ec2_instance" {
     ami = "ami-01f14919ba412de34"
+    
     instance_type = "t2.micro"
+    
     associate_public_ip_address = "true"
+    
     monitoring = true #detailed monitoring 
+    
     count = 2 
+
+    lifecycle {
+        prevent_destroy = true 
+    }
 }
