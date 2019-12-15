491e55e2c4884bf7eef0130f04d0e7ac341d13b8
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-15 02:52:32 -0600

    tags added

--- sec_group.tf
+++ sec_group.tf
@@ -12,11 +12,24 @@ resource "aws_security_group" "sg" {
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 80 #FROM A
+    to_port     = 80 #TO B
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow ssh and http"
   }
 }
